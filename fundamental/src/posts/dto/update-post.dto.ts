f7491ff93b9e0948e1d771465e73dccc190965d4
Nur-A-Alam <nuraalam376@gmail.com>
2026-01-06 12:42:03 +0600

    dto implementation

---
+++ fundamental/src/posts/dto/update-post.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class UpdatePostDto {
+  @IsOptional()
+  @IsNotEmpty({ message: 'Title is required' })
+  @IsString({ message: 'Title must be a string' })
+  @MinLength(3, { message: 'Title must be at least 3 characters' })
+  @MaxLength(50, { message: 'Title can not longer than 50 characters' })
+  title?: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: 'Content is required' })
+  @IsString({ message: 'Content must be a string' })
+  @MinLength(5, { message: 'Content must be at least 5 characters' })
+  @MaxLength(1000, { message: 'Content can not longer than 1000 characters' })
+  content?: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: 'Author Name is required' })
+  @IsString({ message: 'Author Name must be string' })
+  @MinLength(2, { message: 'Author Name must be at least 2 characters' })
+  @MaxLength(25, { message: 'Author Name can not longer than 25 characters' })
+  authorName?: string;
+}
